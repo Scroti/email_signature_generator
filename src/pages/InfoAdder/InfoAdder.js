23158abe59a65111752c639895eab26c36434a74
Cristian M <cristian.mihalache@p3-group.com>
2024-10-08 12:10:13 +0300

    modified

--- src/pages/InfoAdder/InfoAdder.js
+++ src/pages/InfoAdder/InfoAdder.js
@@ -8,39 +8,12 @@ const InfoAdder = () => {
   const [email, setEmail] = useState('');
   const [position, setPosition] = useState('');
   const [name, setName] = useState('');
-  const [officeNr, setOfficeNr] = useState('');
-  const [platforms, setPlatforms] = useState({
-    instagram: false,
-    facebook: false,
-    linkedin: false
-  });
-  const [socialLinks, setSocialLinks] = useState({
-    instagram: '',
-    facebook: '',
-    linkedin: ''
-  });
   const navigate = useNavigate();
-  // Handle checkbox change for selecting platforms
-  const handleCheckboxChange = (e) => {
-    const { name, checked } = e.target;
-    setPlatforms((prevPlatforms) => ({
-      ...prevPlatforms,
-      [name]: checked
-    }));
-  };
-
-  // Handle input change for social links
-  const handleLinkChange = (platform, value) => {
-    setSocialLinks((prevLinks) => ({
-      ...prevLinks,
-      [platform]: value
-    }));
-  };
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ state: email, position, name, officeNr,platforms,socialLinks  })
-    // Navigate to the UserDetails page and pass the form data using state
-    navigate('/user-details', { state:{ email, position, name, officeNr,platforms,socialLinks}});
+    console.log({ state: email, position, name})
+    navigate('/user-details', { state:{ email, position, name}});
   };
 
   return (
@@ -71,78 +44,6 @@ const InfoAdder = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <Input
-          type="text"
-          name="officeNr"
-          label="Office Number"
-          placeholder="Enter your office number"
-          value={officeNr}
-          onChange={(e) => setOfficeNr(e.target.value)}
-        />
-         <div>
-          <h2>Select Social Platforms (Optional)</h2>
-          
-          <label>
-            <input 
-              type="checkbox" 
-              name="instagram" 
-              checked={platforms.instagram} 
-              onChange={handleCheckboxChange}
-            />
-            Instagram
-          </label>
-          {platforms.instagram && (
-            <Input
-              type="text"
-              name="instagram"
-              label="Instagram Username"
-              placeholder="Enter Instagram username"
-              value={socialLinks.instagram}
-              onChange={(e) => handleLinkChange('instagram', e.target.value)}
-            />
-          )}
-          
-          <label>
-            <input 
-              type="checkbox" 
-              name="facebook" 
-              checked={platforms.facebook} 
-              onChange={handleCheckboxChange}
-            />
-            Facebook
-          </label>
-          {platforms.facebook && (
-            <Input
-              type="text"
-              name="facebook"
-              label="Facebook Username"
-              placeholder="Enter Facebook username"
-              value={socialLinks.facebook}
-              onChange={(e) => handleLinkChange('facebook', e.target.value)}
-            />
-          )}
-          
-          <label>
-            <input 
-              type="checkbox" 
-              name="linkedin" 
-              checked={platforms.linkedin} 
-              onChange={handleCheckboxChange}
-            />
-            LinkedIn
-          </label>
-          {platforms.linkedin && (
-            <Input
-              type="text"
-              name="linkedin"
-              label="LinkedIn Username"
-              placeholder="Enter LinkedIn username"
-              value={socialLinks.linkedin}
-              onChange={(e) => handleLinkChange('linkedin', e.target.value)}
-            />
-          )}
-        </div>
-
         <button type="submit" className="login-button">Create your signature</button>
       </form>
     </div>
--- src/pages/Signature/Signature.js
+++ src/pages/Signature/Signature.js
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 import Clipboard from 'clipboard';
 import './Signature.css';
 import logo from '../../assets/logo.png';
-import footer from '../../assets/footer.png'
+import footer from '../../assets/footer.png';
 
 // Social Media Logos
 import instagramLogo from '../../assets/instagram.webp';
@@ -11,14 +11,14 @@ import facebookLogo from '../../assets/facebook.png';
 import linkedinLogo from '../../assets/linkedin.png';
 
 // Add your newly imported icons
-import emailIcon from '../../assets/emailIcon.png';   // email icon
-import phoneIcon from '../../assets/phoneIcon.png';   // phone icon
+import emailIcon from '../../assets/emailIcon.png'; // email icon
+import phoneIcon from '../../assets/phoneIcon.png'; // phone icon
 import addressIcon from '../../assets/addressIcon.png'; // address icon
 
 const Signature = () => {
   const location = useLocation();
-  const { email, position, name, officeNr, platforms, socialLinks } =
-    location.state || {};
+  const officeNr = '+31187745111';
+  const { email, position, name } = location.state || {};
   const [copySuccess, setCopySuccess] = useState('');
 
   useEffect(() => {
@@ -48,93 +48,96 @@ const Signature = () => {
 
         {/* Signature HTML to be copied */}
         <div className="signature-content">
-          <table cellPadding="0" cellSpacing="0" border="0">
+          <table border="0" cellPadding="0" cellSpacing="0" style={{ fontFamily: 'Arial, sans-serif', width: '100%' }}>
             <tbody>
               <tr>
-                <td style={{ paddingRight: '10px' }}>
+                <td style={{ paddingRight: '15px', verticalAlign: 'top' }}>
                   <img src={logo} alt="DS People" style={{ width: '130px' }} />
                 </td>
-                <td
-                  style={{ borderLeft: '2px solid #ddd', paddingLeft: '10px' , width:100}}
-                >
-                  <strong style={{color:'black'}}>{name}</strong>
-                  <br />
-                  {position}
-                  <br />
-                  {/* Email with icon */}
-                  <img
-                    src={emailIcon}
-                    alt="Email Icon"
-                    style={{ width: '15px', marginRight: '5px' }}
-                  />
-                  <a href={`mailto:${email}`} style={{ color: '#0073e6' }}>
-                    {email}
-                  </a>
-                  <br />
-                  {/* Phone number with icon */}
-                  <img
-                    src={phoneIcon}
-                    alt="Phone Icon"
-                    style={{ width: '15px', marginRight: '5px' }}
-                  />
-                  <a href={`tel:${officeNr}`} style={{ color: '#0073e6' }}>
-                  {officeNr}
-                  </a>
-                  <br />
-                  {/* Address with icon */}
-                  <img
-                    src={addressIcon}
-                    alt="Address Icon"
-                    style={{ width: '15px', marginRight: '5px' }}
-                  />
-                  Deltageul 13, 3251NG Stellendam
-                  <br />
+                <td style={{ width: '1.5px', backgroundColor: '#007bff' }}></td>
+                <td style={{ paddingLeft: '15px', verticalAlign: 'top' }}>
+                  <p style={{ margin: 0, fontWeight: 'bold', color: 'black' }}>{name}</p>
+                  <p style={{ margin: '5px 0' }}>{position}</p>
+                  <p style={{ margin: '5px 0' }}>
+                    <img
+                      src={emailIcon}
+                      alt="Email Icon"
+                      style={{ width: '15px', marginRight: '5px', verticalAlign: 'middle' }}
+                    />
+                    <a href={`mailto:${email}`} style={{ color: '#0073e6', textDecoration: 'none' }}>
+                      {email}
+                    </a>
+                  </p>
+                  <p style={{ margin: '5px 0' }}>
+                    <img
+                      src={phoneIcon}
+                      alt="Phone Icon"
+                      style={{ width: '15px', marginRight: '5px', verticalAlign: 'middle' }}
+                    />
+                    <a href={`tel:${officeNr}`} style={{ color: '#0073e6', textDecoration: 'none' }}>
+                      {officeNr}
+                    </a>
+                  </p>
+                  <p style={{ margin: '5px 0' }}>
+                    <img
+                      src={addressIcon}
+                      alt="Address Icon"
+                      style={{ width: '15px', marginRight: '5px', verticalAlign: 'middle' }}
+                    />
+                    Deltageul 13, 3251NG Stellendam
+                  </p>
                 </td>
-                <td className="social-links">
-                  {platforms.instagram && socialLinks.instagram && (
-                    <a
-                      href={socialLinks.instagram}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img src={instagramLogo} alt="Instagram" />
-                    </a>
-                  )}
-
-                  {platforms.facebook && socialLinks.facebook && (
-                    <a
-                      href={socialLinks.facebook}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img src={facebookLogo} alt="Facebook" />
-                    </a>
-                  )}
-
-                  {platforms.linkedin && socialLinks.linkedin && (
-                    <a
-                      href={socialLinks.linkedin}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img src={linkedinLogo} alt="LinkedIn" />
-                    </a>
-                  )}
+                <td style={{ paddingLeft: '15px', verticalAlign: 'top' }}>
+                  <table border="0" cellPadding="0" cellSpacing="0" style={{ fontFamily: 'Arial, sans-serif' }}>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <a href="https://www.instagram.com/d.s.people/" target="_blank" rel="noreferrer">
+                            <img src={instagramLogo} alt="Instagram" className="social-icon" style={{ width: '30px' }} />
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style={{ paddingTop: '10px' }}>
+                          <a href="https://www.facebook.com/d.s.people/" target="_blank" rel="noreferrer">
+                            <img src={facebookLogo} alt="Facebook" className="social-icon" style={{ width: '30px' }} />
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style={{ paddingTop: '10px' }}>
+                          <a href="https://www.linkedin.com/company/ds-people-bv/posts/?feedView=all" target="_blank" rel="noreferrer">
+                            <img src={linkedinLogo} alt="LinkedIn" className="social-icon" style={{ width: '30px' }} />
+                          </a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </td>
-                              
+              </tr>
+              <tr>
+                <td colSpan="6" style={{ paddingTop: '10px' }}>
+                  <a href="https://dspeople.nl/" target="_blank" rel="noreferrer">
+                    <img src={footer} alt="DS People Footer" width={550} style={{ marginTop: '10px' }} />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+              <td colSpan={6} style={{ paddingTop: '10px', fontSize: '7.5px'}}>
+
+              The content of this email is confidential and intended for the recipient specified in message only. It is strictly forbidden to share any part of this message with any third party, without a written consent of the sender. If you received this message by mistake, please reply to this message and follow with its deletion, so that we can ensure such a mistake does not occur in the future.
+
+      </td>
               </tr>
             </tbody>
-
           </table>
-          <a href="https://dspeople.nl/" target='_blank' sTyle={{ color: '#0073e6' }} rel="noreferrer">
-          <img src={footer} height={100} style={{marginTop:10}}/>
-          </a>
         </div>
 
-        {/* Copy Button */}
-        <button className="copy-button">Copy Signature</button>
-        {copySuccess && <p className="copy-success">{copySuccess}</p>}
+       
       </div>
+       {/* Copy Button */}
+       <button className="copy-button">Copy Signature</button>
+        {copySuccess && <p className="copy-success">{copySuccess}</p>}
     </div>
   );
 };
